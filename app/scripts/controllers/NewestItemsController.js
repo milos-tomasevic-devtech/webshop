889701da8f360548a5f5a311aa836f59ba74adf9
Milos Tomasevic <milos.tomasevic@devtechgroup.com>
2014-09-25 16:49:43 +0200

    popbox jquery plugin added

--- app/scripts/controllers/NewestItemsController.js
+++ app/scripts/controllers/NewestItemsController.js
@@ -1,7 +1,17 @@
 angular.module('webshop')
     .controller('NewestItemsController', function($scope, productService){
 
+        $scope.$on('$viewContentLoaded', plugins());
+
+        $scope.items = [];
+
         productService.query(function(data) {
             $scope.newestitems = data;
         });
+
+
+        $scope.addItem = function (id, name, price) {
+
+            $scope.items.push({id: id, name: name, price: price});
+        }
     });
