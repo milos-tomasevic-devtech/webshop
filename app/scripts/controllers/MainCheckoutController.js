1122f370e4f08a3443eae08f61849abb1834c391
Milos Tomasevic <milos.tomasevic@devtechgroup.com>
2014-10-06 16:48:33 +0200

    submitForm directive added

---
+++ app/scripts/controllers/MainCheckoutController.js
@@ -0,0 +1,4 @@
+angular.module('webshop')
+    .controller('MainCheckoutController', function($scope) {
+
+    });
---
+++ app/scripts/directives/submitFormDirective.js
@@ -0,0 +1,45 @@
+angular.module('webshop')
+    .directive('submitForm', ['$parse', '$location', function ($parse, $location) {
+        return {
+
+            restrict: 'A',
+            require: ['submitForm','form'],
+            controller: ['$scope', function($scope) {
+                this.attempted = false;
+                this.setAttempted = function () {
+                    this.attempted = true;
+                }
+            }],
+            compile: function(cElement, cAttributes, transclude) {
+                return {
+                    pre: function(scope, formElement, attributes, controllers) {
+
+                        var submitController = controllers[0];
+
+                        scope.rc = scope.rc || {};
+                        scope.rc[attributes.name] = submitController;
+                    },
+                    post: function(scope, formElement, attributes, controllers) {
+                        var routePath = attributes.submitForm;
+
+                        var submitController = controllers[0];
+                        var formController = (controllers.length > 1) ?
+                            controllers[1] : null;
+
+                        formElement.bind('submit', function (event) {
+                            submitController.setAttempted();
+                            if (!scope.$$phase) scope.$apply();
+
+                            if (!formController.$valid) return false;
+
+                            scope.$apply(function() {
+                                $location.path(routePath);
+
+                            });
+                        });
+                    }
+                }
+            }
+        };
+}]);
+
